ac70282d53c51b4bef4164db5e4aff835ee1dfea
tmyllymaki <tmyllymaki@fastmail.com>
2026-08-05 15:58:21 +0300

    remove 1password overlay

--- machines/desktop/configuration.nix
+++ machines/desktop/configuration.nix
@@ -9,14 +9,6 @@
 
     nixpkgs.overlays = [
       inputs.ts3-noweb.overlays.default
-      (final: prev: {
-        _1password-gui = prev._1password-gui.overrideAttrs (oldAttrs: {
-          src = final.fetchurl {
-            url = "https://downloads.1password.com/linux/tar/stable/x86_64/1password-8.12.21.x64.tar.gz";
-            hash = "sha256-JwiMi2iozP6jWSIUtgXla86aSAhuUob7snqtUbeXPpI=";
-          };
-        });
-      })
     ];
 
     system.stateVersion = "23.05";
